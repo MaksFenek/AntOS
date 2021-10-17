6863ee1fb5580d613ca909d39c63e471d8a3653e
MaksFenek <makstreonin@gmail.com>
2021-10-17 15:19:00 +0300

    Added contact section in menu

--- src/components/menu/MenuList/index.tsx
+++ src/components/menu/MenuList/index.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 import MenuLayout, {MenuItemProps} from 'antd/lib/menu'
 import {App} from 'src/redux/types'
+import {GithubOutlined, FileTextFilled} from '@ant-design/icons'
+
+import TgIcon from 'src/assets/tg.svg'
+import Gmail from 'src/assets/gmail.svg'
 
 const {SubMenu, Item} = MenuLayout
 
@@ -27,13 +31,39 @@ export const MenuList: React.FC<IMenuList> = ({apps, onAppClick}) => {
           </Item>
         ))}
       </SubMenu>
-      <SubMenu key="sub2" title="Navigation Two">
-        <Item key="5">Option 5</Item>
-        <Item key="6">Option 6</Item>
-        <SubMenu key="sub3" title="Submenu">
-          <Item key="7">Option 7</Item>
-          <Item key="8">Option 8</Item>
-        </SubMenu>
+      <SubMenu key="sub2" title="Contacts">
+        <Item icon={<GithubOutlined style={{fontSize: '30px'}} />} key="github">
+          <a
+            target="_blank"
+            rel="nofollow noopener"
+            href="https://github.com/MaksFenek">
+            GitHub
+          </a>
+        </Item>
+        <Item icon={<img src={TgIcon} style={{height: 30}} />} key="tg">
+          <a
+            target="_blank"
+            href="https://t.me/m_fenek"
+            rel="nofollow noopener">
+            Telegram
+          </a>
+        </Item>
+        <Item icon={<FileTextFilled style={{fontSize: '30px'}} />} key="resume">
+          <a
+            target="_blank"
+            href="https://hh.ru/resume/1bb48cb7ff08c91eb20039ed1f3356576a3478"
+            rel="nofollow noopener">
+            Resume
+          </a>
+        </Item>
+        <Item icon={<img src={Gmail} style={{height: 30}} />} key="mail">
+          <a
+            target="_blank"
+            href="mailto:maksteonin@gmail.com"
+            rel="nofollow noopener">
+            Gmail
+          </a>
+        </Item>
       </SubMenu>
     </MenuLayout>
   )
